3ca95627db91d17849ab83ea31a66530732bd82d
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-02-05 15:27:46 +0800

    submit wifi

--- WiFi/src/WiFi.java
+++ WiFi/src/WiFi.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Map;
 
 class WiFi {
 
@@ -6,13 +7,40 @@ class WiFi {
      * Implement your solution here
      */
     public static double computeDistance(int[] houses, int numOfAccessPoints) {
-        return 0.0;
+        int firstHouse = Integer.MAX_VALUE;
+        int lastHouse = Integer.MIN_VALUE;
+        for (int house : houses) {
+            firstHouse = Integer.min(firstHouse, house);
+            lastHouse = Integer.max(lastHouse, house);
+        }
+        double low = 0.0;
+        double high = (lastHouse - firstHouse) / 2;
+        while (high - low > 0.5) {
+            double mid = low + (high - low) / 2;
+            if (WiFi.coverable(houses, numOfAccessPoints, mid)) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        return high;
     }
 
     /**
      * Implement your solution here
      */
     public static boolean coverable(int[] houses, int numOfAccessPoints, double distance) {
-        return false;
+        Arrays.sort(houses);
+        double covered = Double.MIN_VALUE;
+        for (int house : houses) {
+            if (house > covered) {
+                numOfAccessPoints--;
+                if (numOfAccessPoints < 0) {
+                    return false;
+                }
+                covered = house + distance * 2;
+            }
+        }
+        return true;
     }
 }
