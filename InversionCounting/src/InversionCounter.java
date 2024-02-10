da90610f118a8d3c1e668a4dd4dc1f6affc72ba5
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-02-10 23:12:35 +0800

    countinginversions: add mergeandcount

--- InversionCounting/src/InversionCounter.java
+++ InversionCounting/src/InversionCounter.java
@@ -10,6 +10,23 @@ class InversionCounter {
      * minimum amount of adjacent swaps needed to do so.
      */
     public static long mergeAndCount(int[] arr, int left1, int right1, int left2, int right2) {
-        return 0;
+        if (arr[right1] <= arr[left2]) {
+            return 0;
+        }
+        long swaps = 0;
+        while (left1 <= right1 && left2 <= right2) {
+            if (arr[left1] > arr[left2]) {
+                int temp = arr[left2];
+                for (int i = left2; i > left1; i--) {
+                    arr[i] = arr[i - 1];
+                    swaps++;
+                }
+                arr[left1] = temp;
+                right1++;
+                left2++;
+            }
+            left1++;
+        }
+        return swaps;
     }
 }
