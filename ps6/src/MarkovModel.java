11c400187f8bb01d2e42b0839a80a4cc002744e6
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-03-17 17:54:51 +0800

    add ps6

---
+++ ps6/src/MarkovModel.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+/**
+ * This is the main class for your Markov Model.
+ *
+ * Assume that the text will contain ASCII characters in the range [1,255].
+ * ASCII character 0 (the NULL character) will be treated as a non-character.
+ *
+ * Any such NULL characters in the original text should be ignored.
+ */
+public class MarkovModel {
+
+	// Use this to generate random numbers as needed
+	private Random generator = new Random();
+
+	// This is a special symbol to indicate no character
+	public static final char NOCHARACTER = (char) 0;
+
+	/**
+	 * Constructor for MarkovModel class.
+	 *
+	 * @param order the number of characters to identify for the Markov Model sequence
+	 * @param seed the seed used by the random number generator
+	 */
+	public MarkovModel(int order, long seed) {
+		// Initialize your class here
+
+		// Initialize the random number generator
+		generator.setSeed(seed);
+	}
+
+	/**
+	 * Builds the Markov Model based on the specified text string.
+	 */
+	public void initializeText(String text) {
+		// Build the Markov model here
+	}
+
+	/**
+	 * Returns the number of times the specified kgram appeared in the text.
+	 */
+	public int getFrequency(String kgram) {
+		return 0;
+	}
+
+	/**
+	 * Returns the number of times the character c appears immediately after the specified kgram.
+	 */
+	public int getFrequency(String kgram, char c) {
+		return 0;
+	}
+
+	/**
+	 * Generates the next character from the Markov Model.
+	 * Return NOCHARACTER if the kgram is not in the table, or if there is no
+	 * valid character following the kgram.
+	 */
+	public char nextCharacter(String kgram) {
+		// See the problem set description for details
+		// on how to make the random selection.
+		return 'a';
+	}
+}
