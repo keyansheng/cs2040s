c92571fb3dc010be28cf0cca1c98a2d23c8b805c
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-07 12:58:34 +0800

    submit ps7 part 3

--- ps7_part3/src/MazeSolver.java
+++ ps7_part3/src/MazeSolver.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.function.Function;
 
 public class MazeSolver implements IMazeSolver {
@@ -19,6 +20,8 @@ public class MazeSolver implements IMazeSolver {
 	};
 
 	private Maze maze;
+	private int[][] dists;
+	private PriorityQueue<Coord> pq;
 
 	public MazeSolver() {
 		// TODO: Initialize variables.
@@ -27,11 +30,45 @@ public class MazeSolver implements IMazeSolver {
 	@Override
 	public void initialize(Maze maze) {
 		// TODO: Initialize the solver.
+		this.maze = maze;
+		dists = new int[maze.getRows()][maze.getColumns()];
 	}
 
 	@Override
 	public Integer pathSearch(int startRow, int startCol, int endRow, int endCol) throws Exception {
 		// TODO: Find minimum fear level.
+		for (int[] row : dists) {
+			Arrays.fill(row, Integer.MAX_VALUE);
+		}
+		pq = new PriorityQueue<>();
+
+		Coord start = new Coord(startRow, startCol);
+		Coord end = new Coord(endRow, endCol);
+		start.setDist(0);
+		pq.add(start);
+
+		while (!pq.isEmpty()) {
+			Coord curr = pq.remove();
+			if (curr.equals(end)) {
+				return curr.dist();
+			}
+			for (int dir = 0; dir < 4; dir++) {
+				Coord next = curr.move(dir);
+				if (next != null) {
+					int weight = curr.weight(dir);
+					int newDist;
+					try {
+						newDist = Math.addExact(curr.dist(), weight);
+					} catch (ArithmeticException e) {
+						newDist = Integer.MAX_VALUE;
+					}
+					if (next.dist() > newDist) {
+						next.setDist(newDist);
+						pq.add(next);
+					}
+				}
+			}
+		}
 		return null;
 	}
 
@@ -56,6 +93,55 @@ public class MazeSolver implements IMazeSolver {
 			System.out.println(solver.pathSearch(0, 0, 0, 1));
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private class Coord implements Comparable<Coord> {
+		private int row;
+		private int col;
+
+		private Coord(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+
+		private Coord move(int dir) {
+			int newRow = row + DELTAS[dir][0];
+			int newCol = col + DELTAS[dir][1];
+			return 0 <= newRow && newRow < maze.getRows() &&
+					0 <= newCol && newCol < maze.getColumns()
+					? new Coord(newRow, newCol)
+					: null;
+		}
+
+		private int dist() {
+			return dists[row][col];
+		}
+
+		private void setDist(int dist) {
+			dists[row][col] = dist;
+		}
+
+		private Room room() {
+			return maze.getRoom(row, col);
+		}
+
+		private int weight(int dir) {
+			return Math.max(1, WALL_FUNCTIONS.get(dir).apply(room()));
+		}
+
+		@Override
+		public int compareTo(Coord c) {
+			return Integer.compare(dist(), c.dist());
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof Coord) {
+				Coord c = (Coord) o;
+				return row == c.row && col == c.col;
+			}
+			return false;
 		}
 	}
 }
