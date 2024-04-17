139c077bd4e02443a2ab9869decd32bd998c4c34
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-18 03:33:55 +0800

    add medianfinding

---
+++ MedianFinder/src/MedianFinder.java
@@ -0,0 +1,23 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class MedianFinder {
+
+    // TODO: Include your data structures here
+
+    public MedianFinder() {
+        // TODO: Construct/Initialise your data structures here
+    }
+
+    public void insert(int x) {
+        // TODO: Implement your insertion operation here
+    }
+
+    public int getMedian() {
+        // TODO: Implement your getMedian operation here
+
+        return 0;
+    }
+
+
+}
