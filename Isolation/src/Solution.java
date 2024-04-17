ff0cad4d6c25251f4e695eb71a1a6c546080804a
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-18 02:21:10 +0800

    add isolation

---
+++ Isolation/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class Solution {
+    // TODO: Implement your solution here
+    public static int solve(int[] arr) {
+        int maxLength = -1;
+
+        return maxLength;
+    }
+}
