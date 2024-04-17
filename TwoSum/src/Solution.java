174a51bc0e8685b7e8eb5215c5c8268af124f081
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-18 03:26:17 +0800

    submit 2sum

--- TwoSum/src/Solution.java
+++ TwoSum/src/Solution.java
@@ -1,8 +1,22 @@
+import java.util.HashMap;
 
 public class Solution {
     public static int solve(int[] arr, int target) {
         // TODO: Implement your solution here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
 
-        return 0;
+        for (int a : arr) {
+            int b = target - a;
+            int numB = map.getOrDefault(b, 0);
+            if (numB == 0) {
+                map.put(a, map.getOrDefault(a, 0) + 1);
+            } else {
+                map.put(b, numB - 1);
+                count++;
+            }
+        }
+
+        return count;
     }
 }
