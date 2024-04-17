a5c3d7c2cd9fc8d74d94085c5f9a1bfb9f031588
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-17 17:13:59 +0800

    add bellmanford

---
+++ BellmanFord/src/BellmanFord.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class BellmanFord {
+    // DO NOT MODIFY THE TWO STATIC VARIABLES BELOW
+    public static int INF = 20000000;
+    public static int NEGINF = -20000000;
+
+    // TODO: add additional attributes and/or variables needed here, if any
+
+    public BellmanFord(ArrayList<ArrayList<IntPair>> adjList) {
+        // TODO: initialize your attributes here, if any
+    }
+
+    // TODO: add additional methods here, if any
+
+    public int getDistance(int node) { 
+        // TODO: implement your getDistance operation here
+        return 0;
+    }
+
+}
