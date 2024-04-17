c0a5a8bed9919ace65efbbf0899f79c8955af4d6
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-18 02:59:28 +0800

    submit isolation

--- Isolation/src/Solution.java
+++ Isolation/src/Solution.java
@@ -5,6 +5,16 @@ public class Solution {
     // TODO: Implement your solution here
     public static int solve(int[] arr) {
         int maxLength = -1;
+        int begin = -1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                begin = Math.max(begin, map.remove(arr[i]));
+            }
+            maxLength = Math.max(maxLength, i - begin);
+            map.put(arr[i], i);
+        }
 
         return maxLength;
     }
