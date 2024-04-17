c984803b77944f7262c85d923b384debd91f77da
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-18 01:47:34 +0800

    submit bellmanford

--- BellmanFord/src/BellmanFord.java
+++ BellmanFord/src/BellmanFord.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BellmanFord {
     // DO NOT MODIFY THE TWO STATIC VARIABLES BELOW
@@ -6,16 +7,50 @@ public class BellmanFord {
     public static int NEGINF = -20000000;
 
     // TODO: add additional attributes and/or variables needed here, if any
+    private ArrayList<ArrayList<IntPair>> adjList;
+    private int[] dists;
 
     public BellmanFord(ArrayList<ArrayList<IntPair>> adjList) {
         // TODO: initialize your attributes here, if any
+        this.adjList = adjList;
+        dists = new int[adjList.size()];
     }
 
     // TODO: add additional methods here, if any
+    private int addWeight(int v, int weight) {
+        return dists[v] == INF || dists[v] == NEGINF ? dists[v] : dists[v] + weight;
+    }
+
+    private void relaxAll() {
+        relaxAll(false);
+    }
+
+    private void relaxAll(boolean negWeightCycle) {
+        for (int i = 0; i < dists.length; i++) {
+            for (int u = 0; u < dists.length; u++) {
+                ArrayList<IntPair> edges = adjList.get(u);
+                for (IntPair edge : edges) {
+                    int v = edge.first;
+                    int weight = edge.second;
+                    int newDist = addWeight(u, weight);
+                    if (dists[v] > newDist) {
+                        dists[v] = negWeightCycle ? NEGINF : newDist;
+                    }
+                }
+            }
+        }
+    }
+
+    public void computeShortestPaths(int source) {
+        Arrays.fill(dists, INF);
+        dists[source] = 0;
+        relaxAll();
+        relaxAll(true);
+    }
 
     public int getDistance(int node) { 
         // TODO: implement your getDistance operation here
-        return 0;
+        return dists[node];
     }
 
 }
