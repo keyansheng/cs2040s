45636c1afcdf24d0a2c735db21ae08d4ba724dae
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-18 10:07:31 +0800

    add kattisquest

---
+++ KattisQuest/src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    // TODO: Include your data structures here
+
+    public Solution() {
+        // TODO: Construct/Initialise your data structures here
+    }
+
+    void add(long energy, long value) {
+        // TODO: Implement your insertion operation here
+    }
+
+    long query(long remainingEnergy) {
+        // TODO: Implement your query operation here
+
+        return 0L;
+    }
+
+}
