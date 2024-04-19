0b49831daa6ab24053e81ce5d0d520a9402dd124
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-04-19 22:40:14 +0800

    submit sortofsorting

--- SortOfSorting/src/Sorter.java
+++ SortOfSorting/src/Sorter.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 class Sorter {
 
     public static void sortStrings(String[] arr) {
         // TODO: implement your sorting function here
+        Arrays.sort(arr, (str1, str2) -> {
+            String s1 = str1.length() < 2 ? str1 : str1.substring(0, 2);
+            String s2 = str2.length() < 2 ? str2 : str2.substring(0, 2);
+            return s1.compareTo(s2);
+        });
     }
 }
